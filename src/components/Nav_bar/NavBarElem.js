4c27bf39271a135af1b0dee0b171b9175fc10532
dmytro-doroshenko <doroshenko_dima@ukr.net>
2020-03-18 21:49:44 +0200

    buttons done

--- src/components/Nav_bar/NavBarElem.js
+++ src/components/Nav_bar/NavBarElem.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './Nav_bar.css';
 
-function NavBarElem({post, activeID, read}) {
 
+function NavBarElem({post, activeID, read, ind, start, end}) {
+
+    let isShown = false;
 
     const onClickHandler = (id, e) => {
         activeID(id);
@@ -12,9 +14,22 @@ function NavBarElem({post, activeID, read}) {
         read(id)
     };
 
+    if (start <= ind && ind < end) {
+        isShown = true
+    }
+
+
     return(
-            <a href='#' className='list_item' key={post.id} title={post.title} onDoubleClick={onDoubleClickHandler.bind(this, post.id)} onClick={onClickHandler.bind(this, post.id)} >{post.isRead ? '' : <span className='red'>Unread</span>} {post.id}. {post.title}</a>
-        )
+        isShown &&
+            <a
+                href='#' className='list_item'
+                key={post.id} title={post.title}
+                onDoubleClick={onDoubleClickHandler.bind(this, post.id)}
+                onClick={onClickHandler.bind(this, post.id)}
+            >
+                {post.isRead ? '' : <span className='red'>Unread</span>} {post.id}. {post.title}
+            </a>
+    )
 }
 
 export default NavBarElem;
--- src/components/Nav_bar/Nav_bar.js
+++ src/components/Nav_bar/Nav_bar.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
 import './Nav_bar.css';
 import NavBarElem from "./NavBarElem";
 
 function NavBar ({list, change, readTrue}) {
+
+    const [leftBorder, setLeftBorder] = useState(0);
+    const [rightBorder, setRightBorder] = useState(12);
 
     const setActive = (id) => {
         change(id);
@@ -12,12 +15,49 @@ function NavBar ({list, change, readTrue}) {
         readTrue(id)
     };
 
+    const prev = () => {
+        setLeftBorder( leftBorder => leftBorder - 12);
+        setRightBorder( leftBorder);
+    };
+
+    const next = () => {
+        setLeftBorder( rightBorder );
+        setRightBorder( rightBorder => rightBorder + 12);
+    };
+
+    const more = () => {
+        setRightBorder(rightBorder => rightBorder + 12)
+    };
+
     return (
         <div className='navigation'>
             {list
-                ? list.map(item => <NavBarElem key={item.id} post={item} activeID={setActive} read={setRead}/>)
+                ? list.map((item, index) => {
+                        return (
+                            <NavBarElem
+                                key={item.id}
+                                post={item}
+                                ind={index}
+                                start={leftBorder}
+                                end={rightBorder}
+                                activeID={setActive}
+                                read={setRead}
+                            />
+                        )
+                    })
                 : 'Loading...'
             }
+            <div className='d-flex space-around p-10px'>
+                {leftBorder > 0 && <button className='btn' onClick={prev}>Prev. 12</button>}
+                {rightBorder <= list.length &&
+                    <Fragment>
+                        <button className='btn' onClick={more} >More {list.length - rightBorder < 12 ? list.length - rightBorder : '12'}</button>
+                        <button className='btn' onClick={next}>Next {list.length - rightBorder < 12 ? list.length - rightBorder : '12'}</button>
+                    </Fragment>
+
+                }
+
+            </div>
         </div>
     );
 
